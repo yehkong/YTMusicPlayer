14048a6b23e64f4c15f3eca8c33b8b00f77b2c5c
yetaiwen <yetaiwen@yetaiwendeMac-mini.local>
2018-03-18 22:49:21 +0800

    简单使用说明

--- YTMusicPlayer/YTMusicPlayer.h
+++ YTMusicPlayer/YTMusicPlayer.h
@@ -10,15 +10,26 @@
 #import <MediaPlayer/MediaPlayer.h>
 #import <AVFoundation/AVFoundation.h>
 @interface YTMusicPlayer : UIViewController
-
+/*播放器的基本初始化API*/
+//初始化单例
 +(instancetype)shareAVPlayer;
+//指定当前播放的曲目
 @property(nonatomic,strong) id currentItem;
+//曲目列表，可以是URL,也可以是MPMediaItem，这里可以根据扩展音乐类型情况装入不同的音乐来源数据
 @property(nonatomic,strong) NSMutableArray * musicList;
-@property (nonatomic,strong) AVPlayer * musicPlayer;//播放器
+/*播放器的扩展API，可用于全局播放*/
+//播放器
+@property (nonatomic,strong) AVPlayer * musicPlayer;
+//上一曲按钮
 @property (weak, nonatomic) IBOutlet UIButton *lastBtn;
+//下一曲按钮
 @property (weak, nonatomic) IBOutlet UIButton *nextBtn;
+//播放暂停按钮
 @property (weak, nonatomic) IBOutlet UIButton *playBtn;
+//播放暂停
 - (IBAction)playOrPauseAction:(UIButton *)sender;
+//播放下一曲
 - (IBAction)nextSongAction:(UIButton *)sender;
+//播放上一曲
 - (IBAction)lastSongAction:(UIButton *)sender;
 @end
